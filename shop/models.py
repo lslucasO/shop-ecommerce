471d84b053f9f54e0157f8ea38753a07b3354dbe
Lucas Santana Lemos <lslemos59@gmail.com>
2023-10-28 23:22:23 -0300

    creating models

--- shop/models.py
+++ shop/models.py
@@ -1,2 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+
+class BookCover(models.Model):
+    cover = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.cover
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    cover_img = models.ImageField(blank=True, default='')
+    price = models.FloatField()
+    rating = models.FloatField()
+    type_cover = models.ForeignKey(BookCover, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    
+    def __str__(self):
+        return self.title
+    
+    
